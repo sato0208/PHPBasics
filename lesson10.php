65f567f369c04512ba74a3080e70ea1c47ab4e36
sato chiharu <kujira028@gmail.com>
2020-09-05 09:54:58 +0000

    array_map()を使用する

---
+++ lesson10.php
@@ -0,0 +1,16 @@
+
+<?php
+
+$prices = [100, 200, 300];
+
+
+// 10パーセントの税をかけた配列を取得する
+$newPrices = array_map(
+  // 無名関数
+  // function ($n) { return $n * 1.1; },
+  // アロー関数
+  fn($n) => $n * 1.1,
+  $prices
+  );
+  
+print_r($newPrices);
