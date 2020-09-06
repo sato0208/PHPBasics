d1d285fdad0e5db187a6fec47af4ee15931fb529
sato0208 <satochiharu@satonoMacBook-Pro.local>
2020-09-06 13:27:15 +0900

    array_filter()を使用

---
+++ lesson11.php
@@ -0,0 +1,22 @@
+
+<?php
+
+$numbers = range(1, 10);
+
+// trueのもののみ配列に返す
+$evenNumbers = array_filter(
+  // 渡すための配列
+  $numbers,
+  // 条件
+  // function ($n){
+    // if ($n % 2 == 0)
+    //   return true;
+    // } else {
+    //   return false;
+    // }
+    // return $n % === 0;
+  // }
+   fn($n) => $n % 2 === 0
+);
+
+print_r($evenNumbers);
